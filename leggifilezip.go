ecfba0dd7f06bc5acbdff1b1756361987795e97c
axamon <alberto.bregliano@gmail.com>
2017-11-28 00:04:11 +0100

    modificato Time format

--- leggifilezip.go
+++ leggifilezip.go
@@ -132,7 +132,7 @@ func leggizip(file string) {
 			if err != nil {
 				fmt.Println(err)
 			}
-			Time := t.Format(time.RFC3339)
+			Time := t.Format("2006-01-02T15:04:05.000Z")
 			fmt.Println(Time)
 			continue
 		}
