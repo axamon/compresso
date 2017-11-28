82d418267f3d6e1d377fa52ba353ee918d78da13
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2017-11-28 16:49:38 +0100

    aggiunto test

--- leggizip_test.go
+++ leggizip_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/go-redis/redis"
 	"github.com/remeh/sizedwaitgroup"
 )
 
@@ -11,6 +13,18 @@ import (
 // 	// Unordered output: 1
 // 	// [18/Jun/2016:23:59:59.860+0000] http://vodos.oscdn.skycdn.it/RM/live/skycinema/MOVIE/skycinema-MVXY0000000000576770-201606141912150000.nff 185.26.141.212/ 185.26.141.212 41501 41501 979329565 100 5.515 1 206 video/nff No - 44909|HTTP|Sat_Jun_18_23:59:59_2016|0|1 SUCCESS_FINISH - - -
 // }
+
+func ExampleNewClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+}
 
 func BenchmarkLeggizip(b *testing.B) {
 	var wg = sizedwaitgroup.New(200)
