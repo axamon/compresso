cd58383101774476f5a4c8e35b98d831ef2a0788
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2017-11-28 19:25:04 +0100

    test modificato

--- leggizip_test.go
+++ leggizip_test.go
@@ -33,7 +33,7 @@ func ExampleLeggizip() {
 	//var wg = sizedwaitgroup.New(200)
 
 	//wg.Add()
-	leggizip("we_ingestlog_clf_81.74.224.5_20160619_000000_52234.gz")
+	leggizip2("we_ingestlog_clf_81.74.224.5_20160619_000000_52234.gz")
 	//wg.Wait()
 	val, err := client.SCard("recordhashes").Result()
 	if err != nil {
